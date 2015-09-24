5e2db5aa4e41047947ba7e897bb0d12384740675
valerianie <muri.mont@posteo.ch>
2015-09-25 01:42:19 +0200

    I added the second r file

---
+++ Assignment1_file2.R
@@ -0,0 +1,9 @@
+################
+## Valerianie ##
+## 25.09.2015 ##
+## A1 Rfile 2 ##
+################
+
+### read data
+main_data <- read.csv("~/git_repos/first_project/main_data.csv", header=FALSE)
+
