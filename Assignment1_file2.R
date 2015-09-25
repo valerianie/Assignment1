0c2a511cced84b697684add57be2dc843519a0e0
valerianie <muri.mont@posteo.ch>
2015-09-25 02:10:12 +0200

    valerianie

--- Assignment1_file2.R
+++ Assignment1_file2.R
@@ -4,6 +4,5 @@
 ## A1 Rfile 2 ##
 ################
 
-### read data
-main_data <- read.csv("~/git_repos/first_project/main_data.csv", header=FALSE)
-
+### read data into R
+main_data <- read.csv("~/git_repos/assignment_1/Assignment1/main_data.csv", header=FALSE)
