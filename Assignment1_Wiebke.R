d31a971aa4392fe591f84c8b2c2bc76cb92a43c6
wwiebke <w-weiger@msn.com>
2015-09-29 19:35:33 +0200

    Additions to R source file
    basic transformations on the data and illustration of the datas'
    distributions

--- Assignment1_Wiebke.R
+++ Assignment1_Wiebke.R
@@ -4,3 +4,64 @@
 # Wiebke Weiger                             #
 #############################################
 
+# setting the working directory:
+
+setwd("C:\\Users\\Wiebke\\Documents\\Hertie School of Governance\\courses and 
+       classes\\3-Fall Semester 2015\\Gandrud_Introduction to Collaborative 
+       Social Science Data Analysis\\Assignment1") 
+# please change the directory above to your local directory in order to run 
+# this file
+
+################################################################
+# Dataset of interest: Effectiveness of Insect Sprays
+# ##############################################################
+
+data("InsectSprays") # loads the dataset (one of the pre-installed R data sets)
+
+
+### Get a general overview over the dataset:
+
+names(InsectSprays) # gives the names of the two variables: "count" and "spray"
+
+?InsectSprays # gives some general information on the data set:
+# The dataset contains the counts of insects in agricultural experimental units 
+# treated with different insecticides.
+# It's a data frame with 72 observations on 2 variables
+# The variable "count" is the Insect count, it's a numeric variable
+# The variable "spray" is the type of spray, it's a factor variable
+
+view(InsectSprays) # shows the whole dataset as a table in the data viewer
+# the variable "spray" is made up of 6 levels (named A to F)
+
+
+# closer look at the variable "count":
+
+summary(InsectSprays$count)
+#  Min. 1st Qu. Median  Mean  3rd Qu.   Max.
+#  0.00   3.00    7.00  9.50    14.25  26.00
+
+# Plotting a histogram:
+hist(InsectSprays$count) # shows a histogram of "count"
+# histogram shows skewed distribution
+# data contains zeros, therefore not useful to perform natural log transformation
+
+plot(InsectSprays$spray, InsectSprays$count)
+# the plot gives a graphic illustration of the counts of insects treated with 
+# the 6 differen types of sprays
+
+
+# Variation in the data:
+var(InsectSprays$count) # variance: 51.89
+sd(InsectSprays$count)  # standard error: 7.20
+
+
+
+
+
+
+#############################################
+# Citation of Software:
+
+# R Core Team (2015). R: A language and environment for
+# statistical computing. R Foundation for Statistical Computing,
+# Vienna, Austria. URL https://www.R-project.org/.
