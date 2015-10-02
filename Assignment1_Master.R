c585d40f1e08eab15f2aba14c941566ad1a9f9db
Jasmin <jasmincantzler@Jasmins-MacBook-Air.local>
2015-10-02 10:45:46 +0200

    update

--- Assignment1_Master.R
+++ Assignment1_Master.R
@@ -9,7 +9,7 @@
 setwd ('/Users/jasmincantzler/Dropbox/HsoG/4th_Semester/Collaborative_Social_Science_Data_Analysis/Assignment1')
 
 # change the directory above to your local director to run this file through
-# This file runs three different datasets
+# This file runs two different R core datasets
 
 # The first datset looks at the average heights and weights of US women aged 30-39.
 # Please see Assignment1_dofile.R for the R script
@@ -20,8 +20,8 @@ source('Assignment1_dofile.R')
 # Please see Assignment1_Wiebke.R for the R script
 source('Assignment1_Wiebke.R')
 
-# Citation of Software:
 
+# Citation of Software:
 # R Core Team (2015). R: A language and environment for
 # statistical computing. R Foundation for Statistical Computing,
 # Vienna, Austria. URL https://www.R-project.org/.
