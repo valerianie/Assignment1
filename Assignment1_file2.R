55753198e2efe90ca506ef374de67c145267709a
valerianie <muri.mont@posteo.ch>
2015-10-02 10:49:36 +0200

    data description added in r script

--- Assignment1_file2.R
+++ Assignment1_file2.R
@@ -4,8 +4,41 @@
 ###    date: "25.09.2015"     ###
 #################################
 
-#read data into R
+#####################
+#Read Data into R
+#####################
 main_data <- read.csv("~/git_repos/assignment_1/Assignment1/main_data.csv", header=FALSE)
 
-#link files with source command
+#####################
+#Link Files with Source Command
+#####################
 source('Assignment1_dofile.R')
+
+#####################
+#Data Set
+#####################
+
+View(main_data)
+names(main_data)
+names(main_data) <- c("iso2c","year","country", "fertilizer_consumption", "usd")
+
+summary(main_data$fertilizer_consumption)
+
+main_data$fertilizer_consumption<-as.numeric(main_data$fertilizer_consumption)
+?main_data
+
+mean(main_data$fertilizer_consumption)
+
+
+
+
+
+#####################
+#Citation of Software
+#####################
+
+citation()
+
+## R Core Team (2014). R: A language and environment for
+## statistical computing. R Foundation for Statistical Computing,
+## Vienna, Austria. URL http://www.R-project.org/.
